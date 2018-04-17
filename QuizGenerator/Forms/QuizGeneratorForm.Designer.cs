219d0a9b2316052a02569ef9613add5090724e11
Isur <arturbed96@gmail.com>
2018-04-17 23:30:45 +0200

    g

--- QuizGenerator/Forms/QuizGeneratorForm.Designer.cs
+++ QuizGenerator/Forms/QuizGeneratorForm.Designer.cs
@@ -30,8 +30,6 @@
         {
             this.textBoxQuizName = new System.Windows.Forms.TextBox();
             this.labelQuizName = new System.Windows.Forms.Label();
-            this.userControlAnswer1 = new QuizGenerator.UserControls.userControlAnswer();
-            this.userControlAnswer2 = new QuizGenerator.UserControls.userControlAnswer();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -43,11 +41,16 @@
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.userControlAnswer2 = new QuizGenerator.UserControls.userControlAnswer();
+            this.userControlAnswer1 = new QuizGenerator.UserControls.userControlAnswer();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBoxQuizName
             // 
-            this.textBoxQuizName.Location = new System.Drawing.Point(89, 15);
+            this.textBoxQuizName.Location = new System.Drawing.Point(89, 9);
             this.textBoxQuizName.Name = "textBoxQuizName";
             this.textBoxQuizName.Size = new System.Drawing.Size(183, 20);
             this.textBoxQuizName.TabIndex = 0;
@@ -55,29 +58,15 @@
             // labelQuizName
             // 
             this.labelQuizName.AutoSize = true;
-            this.labelQuizName.Location = new System.Drawing.Point(13, 18);
+            this.labelQuizName.Location = new System.Drawing.Point(5, 9);
             this.labelQuizName.Name = "labelQuizName";
             this.labelQuizName.Size = new System.Drawing.Size(70, 13);
             this.labelQuizName.TabIndex = 1;
             this.labelQuizName.Text = "Nazwa Quizu";
             // 
-            // userControlAnswer1
-            // 
-            this.userControlAnswer1.Location = new System.Drawing.Point(16, 113);
-            this.userControlAnswer1.Name = "userControlAnswer1";
-            this.userControlAnswer1.Size = new System.Drawing.Size(256, 22);
-            this.userControlAnswer1.TabIndex = 2;
-            // 
-            // userControlAnswer2
-            // 
-            this.userControlAnswer2.Location = new System.Drawing.Point(16, 141);
-            this.userControlAnswer2.Name = "userControlAnswer2";
-            this.userControlAnswer2.Size = new System.Drawing.Size(256, 22);
-            this.userControlAnswer2.TabIndex = 3;
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(206, 84);
+            this.button1.Location = new System.Drawing.Point(206, 64);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(30, 23);
             this.button1.TabIndex = 4;
@@ -86,7 +75,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(242, 84);
+            this.button2.Location = new System.Drawing.Point(242, 64);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(30, 23);
             this.button2.TabIndex = 5;
@@ -95,16 +84,16 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(197, 325);
+            this.button3.Location = new System.Drawing.Point(178, 325);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(94, 23);
             this.button3.TabIndex = 6;
-            this.button3.Text = "Zapisz";
+            this.button3.Text = "Zapisz Quiz";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(89, 42);
+            this.textBox1.Location = new System.Drawing.Point(89, 35);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(183, 20);
             this.textBox1.TabIndex = 7;
@@ -112,7 +101,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 45);
+            this.label1.Location = new System.Drawing.Point(5, 38);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(45, 13);
             this.label1.TabIndex = 8;
@@ -121,7 +110,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 89);
+            this.label2.Location = new System.Drawing.Point(5, 74);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(63, 13);
             this.label2.TabIndex = 9;
@@ -130,7 +119,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 73);
+            this.label3.Location = new System.Drawing.Point(5, 61);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(65, 13);
             this.label3.TabIndex = 10;
@@ -138,18 +127,18 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(158, 296);
+            this.button4.Location = new System.Drawing.Point(178, 296);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(114, 23);
+            this.button4.Size = new System.Drawing.Size(94, 23);
             this.button4.TabIndex = 11;
             this.button4.Text = "Zapisz pytanie";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(38, 296);
+            this.button5.Location = new System.Drawing.Point(8, 296);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(114, 23);
+            this.button5.Size = new System.Drawing.Size(94, 23);
             this.button5.TabIndex = 12;
             this.button5.Text = "Usuń pytanie";
             this.button5.UseVisualStyleBackColor = true;
@@ -171,11 +160,55 @@
             this.comboBox1.Size = new System.Drawing.Size(174, 21);
             this.comboBox1.TabIndex = 14;
             // 
+            // userControlAnswer2
+            // 
+            this.userControlAnswer2.Location = new System.Drawing.Point(16, 121);
+            this.userControlAnswer2.Name = "userControlAnswer2";
+            this.userControlAnswer2.Size = new System.Drawing.Size(256, 22);
+            this.userControlAnswer2.TabIndex = 3;
+            // 
+            // userControlAnswer1
+            // 
+            this.userControlAnswer1.Location = new System.Drawing.Point(16, 93);
+            this.userControlAnswer1.Name = "userControlAnswer1";
+            this.userControlAnswer1.Size = new System.Drawing.Size(256, 22);
+            this.userControlAnswer1.TabIndex = 2;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(145, 296);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(31, 23);
+            this.button7.TabIndex = 15;
+            this.button7.Text = "-->";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(108, 296);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(31, 23);
+            this.button8.TabIndex = 16;
+            this.button8.Text = "<--";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(8, 325);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(94, 23);
+            this.button9.TabIndex = 17;
+            this.button9.Text = "Usuń Quiz";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
             // QuizGeneratorForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 396);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
@@ -215,5 +248,8 @@
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
     }
 }
